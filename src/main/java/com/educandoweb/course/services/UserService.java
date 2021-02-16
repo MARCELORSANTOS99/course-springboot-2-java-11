caa1289b37c747dbf0960f17c8b5e198805e6d4e
Marcelo Santos <marcelo.r.santos99@hotmail.com>
2021-02-16 11:42:36 -0300

    User Delete

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -27,9 +27,13 @@ public class UserService {
 		
 	}
 	
-	
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
 	
+	
+	public void delete(Long id) {
+		 repository.deleteById(id);
+	}
 }
+
